7c0d3f9cd79e6e92c873cfd088fdaba20cd7f332
hemanthkb <38094047+hemanthkb@users.noreply.github.com>
2018-05-18 13:39:19 +0530

    Update App.java

--- src/main/java/hemanth/App.java
+++ src/main/java/hemanth/App.java
@@ -9,10 +9,9 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-<<<<<<< HEAD
-	System.out.println( "this is hemanth world welcome" );
-=======
-	System.out.println( "welcome to my world" ):
->>>>>>> master
+	    System.out.println( "welcome to my world" );
+          System.out.println( "this is hemanth world welcome" );
+	
+
     }
 }
